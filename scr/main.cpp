d1ccbf7764da0d0c3705ebc554217e59026b4d96
Catalina Jiggins <catjiggins@gmail.com>
2026-02-28 22:14:23 +0000

    Remove all .DS_Store files

--- scr/main.cpp
+++ scr/main.cpp
@@ -1,3 +1,26 @@
-
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <cassert>
+
+int main()
+{
+    std::string dna;
+    std::string complement = "";
+
+    std::cout << "Enter DNA sequence: ";
+    std::cin >> dna;
+    for(char base : dna)
+    {
+        if(base == 'A') complement += 'T';
+        else if(base == 'T') complement += 'A';
+        else if(base == 'G') complement += 'C';
+        else if(base == 'C') complement += 'G';
+    }
+
+    std::cout << "Complement strand: "
+              << complement << std::endl;
+
+    return 0;
+  
+}
